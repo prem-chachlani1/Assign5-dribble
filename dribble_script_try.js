a87b08e99beda59c3e891cb820b986acc22e254b
Prem Chachlani <prem.chachlani@adapty.com>
2018-12-24 12:17:27 +0530

    24-12-18 morning

--- dribble_script_try.js
+++ dribble_script_try.js
@@ -1,141 +1,151 @@
+
 $(document).ready(function() {
-    var $regexFname = /^[a-zA-Z]{3,16}$/i;
-    var $regexLname = /^[a-zA-Z]{3,16}$/i; /* for D'souza /^[a-zA-z ,.'-]{3,16}$/i;*/
-    var $regexEmailAddr = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/i;
-    var $regexPhoneNo = /^[0-9]{10}$/;
+  var $regexFname = /^[a-zA-Z]{3,16}$/i;
+  var $regexLname = /^[a-zA-Z]{3,16}$/i; /* for D'souza /^[a-zA-z ,.'-]{3,16}$/i;*/
+  var $regexEmailAddr = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/i;
+  var $regexPhoneNo = /^[0-9]{10}$/;
     var $regexPassword = /^[a-zA-Z]\w{3,14}$/; //eg: aBc45DSD_sdf   
     var $regexDOB = /^[0,1]?\d{1}\/(([0-2]?\d{1})|([3][0,1]{1}))\/(([1]{1}[9]{1}[9]{1}\d{1})|([2-9]{1}\d{3}))$/;
-    //var $regexAddr = /^\d{1,5}\s\w.\s(\b\w*\b\s){1,2}\w*\.$/;
-    var $regexAddr = /\s/g;
-    //var $regexPhoneNo = /^\d*[0-9](|.\d*[0-9]|,\d*[0-9])?$/;
-    var invalidStr = "Invalid";
-    //validate text fields
-    function validate(fieldId, msg, minLen, maxLen, regExp) {
-        $(fieldId).focusout(function() {
-            var $this = $(this);
-            var $fieldVal = $this.val();
-            var $fieldLen = $this.val().length;
+    var $regexAddr = /\S+/;
+    
+    var invalidStr = "Invalid ";
+    var  selectMsg = "Please select ";    //validate text fields
+    function validateTextField(fieldId, msg, minLen, maxLen, regExp) {
+      $(fieldId).focusout(function() {
+        var $this = $(this);
+        var $fieldVal = $this.val();
+        var $fieldLen = $this.val().length;
 
-            if ($this === $("#confirm-password").val()) {
-                console.log("inside confirm pwd");
-                if ($this.val() === $("#form-el #password").val()) {
-                    $this.next('.error-msg').hide();
-                    //$this.removeClass('error-msg');
-                } else {
-                    $this.next('.error-msg').text(msg).show();
-                    //$this.addClass('error-msg');
-                }
-            } else {
-
-                if (regExp.test($fieldVal) && $fieldLen >= minLen && $fieldLen <= maxLen) {
-                    //console.log("yeah!!!");
-                    $this.next('.error-msg').css({});
-                    //$this.removeClass('error-msg');
-                } else {
-                    $this.next('.error-msg').text(msg).show();
-                    //console.log("fieldLen ", $fieldLen, "minLen ", minLen, "maxLen ", maxLen);
-                    //$this.addClass('error-msg');
-                }
+        if ($this === $("#form-id #confirm-password").val()) {  
+          console.log("inside confirm pwd");
+          if ($this.val() === $("#form-id #password").val()) {
+            $this.next('.error-msg').css('visibility', 'hidden');
+                  } 
+                  else {  
+                    $this.next('.error-msg').text(msg).css('visibility', 'visible');
+                    
+                  }
+                } 
+                else {
+                  if (regExp.test($fieldVal) && $fieldLen >= minLen && $fieldLen <= maxLen) {
+               
+              $this.next('.error-msg').css('visibility', 'hidden');
+              
+            } 
+            else {
+              $this.next('.error-msg').text(msg).css('visibility', 'visible');
+              
             }
+          }
         }); //focusout
-    } //validate
+    } //validateTextField func end
+    
 
-    validate("#fname", invalidStr + " First name ", 3, 20, $regexFname);
-    validate("#lname", invalidStr + " Last name ", 3, 20, $regexLname);
-    validate("#emailaddr", invalidStr + " Email Address ", 4, 254, $regexEmailAddr);
-    validate("#phone-no", invalidStr + " Phone number ", "10", "10", $regexPhoneNo);
-    validate("#password", invalidStr + " Password ", "7", "15", $regexPassword);
-    validate("#confirm-password", invalidStr + " Confirm Password ", "7", "15", $regexPassword);
-    validate("#dob", invalidStr + " Date of Birth", "10", "10", $regexDOB);
-    validate("#addr", invalidStr + " Address", "5", "72", $regexAddr);
+    validateTextField("#fname", invalidStr + "First name ", 3, 20, $regexFname);
+    validateTextField("#lname", invalidStr + "Last name ", 3, 20, $regexLname);
+    validateTextField("#emailaddr", invalidStr + "Email Address ", 4, 254, $regexEmailAddr);
+    validateTextField("#phone-no", invalidStr + "Phone number ", "10", "10", $regexPhoneNo);
+    validateTextField("#password", invalidStr + "Password ", 7, 15, $regexPassword);
+    validateTextField("#confirm-password", invalidStr + "Confirm Password ", "7", "15", $regexPassword);
+    validateTextField("#dob", invalidStr + "Date of Birth", 10, 10, $regexDOB);
+    validateTextField("#addr", invalidStr + "Address", 5, 72, $regexAddr);
 
     $.ajax({
-        type: "GET",
-        url: 'http://localhost:3000/person',
-        success: function(result) {
-            var tableContainer = $(".table-container");
-            console.log("GET result: ", result);
+      type: "GET",
+      url: 'http://localhost:3000/person',
+      success: function(result) {
+        var tableContainer = $(".table-container");
+        console.log("GET result: ", result);
 
-            var output =
-                "<table><thead><tr><th>First Name</th><th>Last Name</th><th>Email Address</th>" +
-                "<th>State</th><th>Date of Birth</th><th>Address</th>" +
-                "<th>Gender</th><th>Vehicles owned</th> </thead><tbody>";
+        var output =
+        "<table><thead><tr><th>First Name</th><th>Last Name</th><th>Email Address</th>" +
+        "<th>State</th><th>Date of Birth</th><th>Address</th>" +
+        "<th>Gender</th><th>Vehicles owned</th> </thead><tbody>";
 
-            for (var prop in result) {
-                delete result[prop].password;
-                delete result[prop].id;
-                output = output + "<tr>";
-                for (var key in result[prop]) {
-                    if (result[prop].hasOwnProperty(key)) {
-                        output = output + "<td>" + result[prop][key] + "</td>";
-                    }
-                }
-                output = output + "</tr>";
+        for (var prop in result) {
+          delete result[prop].password;
+          delete result[prop].id;
+          output = output + "<tr>";
+          for (var key in result[prop]) {
+            if (result[prop].hasOwnProperty(key)) {
+              output = output + "<td>" + result[prop][key] + "</td>";
             }
-            output = output + "</tbody></table>";
-            tableContainer.html(output);
-            $("table").addClass("form-entries");
-        }
+          }
+                output = output + "</tr>"; // add tr tr
+              }
+              output = output + "</tbody></table>";
+              tableContainer.html(output);
+              $("table").addClass("form-entries");
+        } // success func and add error function
     }); //ajax get
 
 
     //submit button
     $("#request-button").on("click", function(e) { //use bind click
-        e.preventDefault();
-        console.log("working!!!");
-        var vehicleArr = [];
+      e.preventDefault();
+      console.log("working!!!");
 
-        $.each($("input[name='vehicle']:checked"), function() {
-            vehicleArr.push($(this).val());
-        });
+      var isGenderChecked = $('#form-id input[name=gender]:checked');
+       if (isGenderChecked.length > 0) {          
+          $('#form-id .single-option .error-msg').css('visibility', 'hidden');
+      }
+      else {          
+        $('#form-id .single-option .error-msg').css('visibility', 'visible');
+      }
 
-        var person = {
-            fname: $("#fname").val(),
-            lname: $("#lname").val(),
-            emailaddr: $("#emailaddr").val(),
-            state: $("#state").children("option:selected").val(),
-            dob: $("#dob").val(),
-            addr: $("#addr").val(),
-            gender: $("input[name='gender']:checked").val(),
-            vehicles: vehicleArr.join(", "),
-            password: $("#password").val()
-        };
-        console.log("person obj fname: ", person["fname"]);
-        var jsonData = JSON.stringify(person);
+      var vehicleArr = [];
+
+      $.each($("input[name='vehicle']:checked"), function() {
+        vehicleArr.push($(this).val());
+      });
+
+      var person = {
+        fname: $("#fname").val(),
+        lname: $("#lname").val(),
+        emailaddr: $("#emailaddr").val(),
+        state: $("#state").children("option:selected").val(),
+        dob: $("#dob").val(),
+        addr: $("#addr").val(),
+        gender: $("input[name='gender']:checked").val(),
+        vehicles: vehicleArr.join(", "),
+        password: $("#password").val()
+      };
+      console.log("person obj fname: ", person["fname"]);
+      var jsonData = JSON.stringify(person);
 
         //ajax post
         $.ajax({
-            url: 'http://localhost:3000/person',
-            type: 'post',
-            dataType: 'json',
-            contentType: 'application/json',
-            data: jsonData,
-            success: function(result) {
-                console.log("POST result", result);
-                var output = "<tr>";
-                console.log(">> result", result);
-                delete result.password;
-                delete result.id;
-                for (var prop in result) {
-                    if (result.hasOwnProperty(prop)) {
-                        output = output + "<td>" + result[prop] + "</td>";
-                    }
-                }
-                output = output + "</tr>";
-                $(".form-entries").append(output);
-
-            },
-            error: function(xhr) {
-                alert("An error occured while post request: " + xhr.status + " " + xhr.statusText);
+          url: 'http://localhost:3000/person',
+          type: 'post',
+          dataType: 'json',
+          contentType: 'application/json',
+          data: jsonData,
+          success: function(result) {
+            console.log("POST result", result);
+            var output = "<tr>";
+            console.log(">> result", result);
+            delete result.password;
+            delete result.id;
+            for (var prop in result) {
+              if (result.hasOwnProperty(prop)) {
+                output = output + "<td>" + result[prop] + "</td>";
+              }
             }
+            output = output + "</tr>";
+            $(".form-entries").append(output);
+
+          },
+          error: function(xhr) {
+            alert("An error occured while post request: " + xhr.status + " " + xhr.statusText);
+          }
         }); //ajax post
 
     }); //request button
 
     $("#dob").datepicker({
-        maxDate: '0',
-        /*setting today's date as max date*/
-        minDate: new Date(2017, 12 - 1, 1) /*params: year, month, day, hour, minute, second, millisecond*/
+      maxDate: '0',
+      /*setting today's date as max date*/
+      minDate: new Date(2017, 12 - 1, 1) /*params: year, month, day, hour, minute, second, millisecond*/
     });
 
-});
+  });
